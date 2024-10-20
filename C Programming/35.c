6f01701e8198398d4fe55f7d775ee022c5d0ccff
pandya kartik <pandyakartik66@gmail.com>
2024-10-20 20:18:48 +0530

    35.c

--- C Programming/35.c
+++ C Programming/35.c
@@ -1,14 +1,4 @@
 #include <stdio.h>
-int factorial (int n)
- {
-
-    if (n == 0 || n == 1) 
-    {
-        return 1;
-    }
-
-    return n * factorial(n - 1);
-}
 int main() 
 {
     int number;
@@ -18,12 +8,20 @@ int main()
     if (number < 0) 
     {
         printf("Factorial is not defined for negative numbers.\n");
-    } else 
+    } 
+    else 
     {
-
         printf("Factorial of %d is: %d\n", number, factorial(number));
     }
-    
 
     return 0;
+}
+int factorial(int n) 
+{
+    if (n == 0 || n == 1) 
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
 }
