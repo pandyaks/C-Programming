0ba3e0c9a64d2c5b524629a183470577070051b8
pandya kartik <pandyakartik66@gmail.com>
2024-10-20 20:08:58 +0530

    32.c

--- C Programming/32.c
+++ C Programming/32.c
@@ -1,32 +1,27 @@
+// Write a program to find out the max number from given array using function
 #include <stdio.h>
-int findMax(int arr[], int size) 
-{
-    if (size == 0) 
-    {
-        return -1; 
-    }
-    int maxNum = arr[0];
-    for (int i = 1; i < size; i++) 
-    {
-        if (arr[i] > maxNum) 
-        {
-            maxNum = arr[i];
-        }
-    }
-    return maxNum;
-}
+int findMax(int arr[], int size) ,i ;
 int main() 
 {
-    int numbers[100] = {14,78,90,56,4};
-    int size = sizeof(numbers) / sizeof(numbers[0]);
-    int maxNumber = findMax(numbers, size);
-    if (maxNumber != -1) 
-    {
-        printf("The maximum number in the array is: %d\n", maxNumber);
-    } else 
-    {
-        printf("The array is empty.\n");
-    }
+    int size;
+    printf("\nEnter the number of elements in the array: ");
+    scanf("%d", &size);
+    int arr[size];
 
-    return 0;
+    printf("\nEnter %d elements:", size);
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int max = arr[0];  
+
+    for (int i = 1; i < size; i++) 
+    {
+        if (arr[i] > max) 
+        {
+            max = arr[i];  
+        }
+    }
+    printf("\nThe  number in the array is: %d", max);
+   
 }
